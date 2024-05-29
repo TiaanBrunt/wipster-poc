10e689cb03a2ee9f306a0af02e9cb6a4899fb3bc
tiaanbrunt <tiaanbrunt@gmail.com>
2024-05-29 21:04:50 +1200

    Feat: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ export default function RootLayout({
   return (
     
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="https://www.wipster.io/hubfs/favicon-1.png"/>
+      </head>
       <body>{children}</body>
     </html>
   );
